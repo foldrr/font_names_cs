13864f83e1fc68736b5e5d42d7fa8568f2b716ac
EMPTY <EMPTY>
2015-02-16 16:32:32 +0900

    Initial commit.

---
+++ FontNames/Bootstrap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+class Bootstrap {
+    static void Main(string[] args) {
+        var cultureId = CultureInfo.GetCultureInfo("en-us").LCID;
+
+        var collection = new System.Drawing.Text.InstalledFontCollection();
+        var families = collection.Families;
+        foreach(var f in families) {
+            Console.WriteLine(f.GetName(cultureId));
+        }
+    }
+}
